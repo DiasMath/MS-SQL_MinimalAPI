af767291f4ef8da6949e8442459e83f5bf504145
Matheus Dias <92406256+DiasMath@users.noreply.github.com>
2023-09-11 20:48:22 -0300

    adicionando script pos implementacao e adicionando publicacao de banco

---
+++ UserDB/obj/Debug/postdeploy.sql
@@ -0,0 +1,9 @@
+﻿if not exists (SELECT 1 FROM dbo.[User])
+begin
+	INSERT INTO dbo.[User] (FirstName, LastName)
+	VALUES ('Tim','Corey'),
+	('Manua','Losh'),
+	('Nick','Furry'),
+	('Tom','Jerry')
+end
+GO
